dc9dd584d86ee89cb5e55522a3bc1ef9f02cabd4
Muarif Shaikh <muarif.s@nimapinfotech.com>
2024-05-30 19:12:47 +0530

    Add project files.

---
+++ ViewTempAndSession/Controllers/HomeController1.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ViewTempAndSession.Controllers
+{
+    public class HomeController1 : Controller
+    {
+        public IActionResult Index()
+        {   
+            ViewBag.VAR = "tHIS IS View Bag";
+            ViewData["var2"] = "This is View DATA";
+            TempData["var3"] = "This is Temp Data";
+            HttpContext.Session.SetString("Var4", "This is Session Data");
+            return RedirectToAction("About");
+        }
+        public IActionResult About()
+        {
+           
+            return View();
+        }
+    }
+}
